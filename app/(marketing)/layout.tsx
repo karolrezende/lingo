889f2a8bc9098de9b641a1bcf67639a6531ddcb6
karol <karolrds01@gmail.com>
2024-04-07 23:50:31 -0400

    feat: first commit

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./header";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+        <Header/>
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
