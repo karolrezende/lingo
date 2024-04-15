c7ea401e46d1fa193f9833ed022c9d66cf7d2aab
karol <karolrds01@gmail.com>
2024-04-14 23:15:06 -0400

    feat/ adding landingpage

--- app/(landingpage)/header.tsx
+++ app/(landingpage)/header.tsx
@@ -1,9 +1,52 @@
-export const Header = () => {
-    return (
-        <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            <div className=" lg:max-w-screen mx-auto flex items-center justify-between h-full bg-red-500">
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
 
-            </div>
-        </header>
-    )
-}
+export const Header = () => {
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className=" lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src="/logo.svg"
+            height={40}
+            width={40}
+            alt="Mascot"
+            className="select-none"
+          />
+          <h1 className="text-2xl font-extrabold text-pink-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/"/>
+          </SignedIn>
+          <SignedOut>
+            <SignUpButton
+              mode="modal"
+              afterSignInUrl="/learn"
+              afterSignUpUrl="/learn"
+            >
+                <Button size={'lg'} variant={'ghost'}>
+                    Login
+                </Button>
+            </SignUpButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
+    </header>
+  );
+};
--- app/(landingpage)/page.tsx
+++ app/(landingpage)/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button variant={"super"}>click</Button>
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px]  mb-8 lg:mb-0">
+        <Image src={'/hero.svg'} fill alt='hero'/>
+      </div>
+    </div>
   )
 }
